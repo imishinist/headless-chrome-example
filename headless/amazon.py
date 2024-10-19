3c48a733f7671adb12f55be673acee14f51db0ce
Taisuke Miyazaki <imishinist@gmail.com>
2024-10-19 14:39:47 +0900

    use uv and fix

--- headless/amazon.py
+++ headless/amazon.py
@@ -18,6 +18,7 @@ class Amazon:
                 "%2F2.0%2Fidentifier_select&openid.assoc_handle=jpflex&openid.mode=checkid_setup&openid.claimed_id" \
                 "=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid" \
                 ".net%2Fauth%2F2.0& "
+    ORDER_HISTORY_URL = "https://www.amazon.co.jp/gp/css/order-history"
 
     def __init__(self, driver: webdriver.Chrome):
         self.__driver = driver
@@ -48,6 +49,41 @@ class Amazon:
         self.__driver.find_element(By.ID, 'auth-signin-button').submit()
         return True
 
+    def orders(self):
+        self.__driver.get(self.ORDER_HISTORY_URL)
+
+        i = 0
+        elements = self.__driver.find_elements(By.CLASS_NAME, 'hide-if-no-js')
+        for element in elements:
+            i += 1
+            element.click()
+            try:
+                self.__driver.implicitly_wait(3)
+                content = self.__driver.find_element(By.ID, 'a-popover-content-{}'.format(i))
+                self.__driver.implicitly_wait(3)
+                content.find_element(By.CLASS_NAME, 'a-link-normal')
+            except TimeoutException:
+                return False
+
+        links = []
+        elements = self.__driver.find_elements(By.CLASS_NAME, 'a-popover-content')
+        for element in elements:
+            link = element.find_elements(By.CLASS_NAME, 'a-link-normal')
+            if len(link) == 0:
+                continue
+            payment_link = link[-1].get_attribute('href')
+            links.append(payment_link)
+
+        for (idx, link) in enumerate(links):
+            self.__driver.get(link)
+            try:
+                wait = WebDriverWait(self.__driver, 10)
+                wait.until(EC.presence_of_all_elements_located)
+                self.__driver.execute_script("document.title = \'{}.pdf\'".format(idx+1))
+                self.__driver.execute_script('window.print()')
+            except TimeoutException:
+                return False
+
     @staticmethod
     def __validate(email, password):
         if email is None or password is None:
@@ -69,11 +105,29 @@ class Amazon:
             self.__driver.add_cookie(cookie)
 
 
-def generate_driver():
+def generate_driver(download_dir: str):
     options = Options()
     options.headless = False
     options.incognito = True
     options.hide_scrollbars = True
+    appState = {
+        "recentDestinations": [
+            {
+                "id": "Save as PDF",
+                "origin": "local",
+                "account": ""
+            }
+        ],
+        "selectedDestinationId": "Save as PDF",
+        "version": 2,
+        "pageSize": 'A4'
+    }
+    options.add_experimental_option("prefs", {
+        "printing.print_preview_sticky_settings.appState":
+            json.dumps(appState),
+        "savefile.default_directory": download_dir
+    })
+    options.add_argument('--kiosk-printing')
 
     return webdriver.Chrome(options=options)
 
@@ -83,12 +137,19 @@ def main():
     amazon_password = os.environ.get('AMAZON_PASSWORD')
     amazon_totp_secret = os.environ.get('AMAZON_TOTP_SECRET')
 
-    driver = generate_driver()
+
+    import sys
+    download_dir = 'downloads'
+    download_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../' + download_dir))
+    print(download_dir)
+    driver = generate_driver(download_dir + '/')
 
     amazon = Amazon(driver)
     amazon.login(amazon_email, amazon_password, amazon_totp_secret)
 
-    time.sleep(10)
+    amazon.orders()
+
+    time.sleep(2)
     driver.close()
 
 
